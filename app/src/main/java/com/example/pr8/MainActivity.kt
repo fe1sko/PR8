81e45d57296a10d4d8c4fc8705fd81c55c006397
Your Name <you@example.com>
2023-09-28 17:00:19 +0300

    Initial commit

---
+++ app/src/main/java/com/example/pr8/MainActivity.kt
@@ -0,0 +1,34 @@
+package com.example.pr8
+
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+
+private infix fun Nothing?.void(clickMyBtn: Any) {
+
+}
+
+class MainActivity : AppCompatActivity() {
+    var myTextView: TextView? = null
+    var myButton1: Button? = null
+    var myButton2: Button? = null
+    var myButton3: Button? = null
+    var myButton4: Button? = null
+    var myButton5: Button? = null
+    var myButton6: Button? = null
+    var myButton7: Button? = null
+    var myButton8: Button? = null
+    var myButton9: Button? = null
+    var myButton10: Button? = null
+    var myButton11: Button? = null
+    var myButton12: Button? = null
+    var myButton13: Button? = null
+    var myButton14: Button? = null
+    var myButton15: Button? = null
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        var public: Nothing? = null
+        }
+    }
